9c218b6a2ced98dcc396e82c469c53a38e65ea62
Micah Pawlicki <mich.pawlicki@gmail.com>
2024-10-14 11:48:51 -0500

    Player movement

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import player
 
 clock = pygame.time.Clock()
 dt = 0
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
 
 def main():
     pygame.init()
@@ -16,11 +18,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("black")
+        p1.update(dt)
         p1.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
