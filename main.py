04d56a5ed24c5002235a68e9b664d04499529169
Micah Pawlicki <mich.pawlicki@gmail.com>
2024-10-14 12:07:12 -0500

    created sprite groups

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import pygame
 from constants import *
-import player
-
-clock = pygame.time.Clock()
-dt = 0
-updatable = pygame.sprite.Group()
-drawable = pygame.sprite.Group()
+from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    p1 = player.Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
+
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        p1.update(dt)
-        p1.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
