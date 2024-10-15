529aab90265a7f685f6e7f167548ebebd94ccc90
Micah Pawlicki <mich.pawlicki@gmail.com>
2024-10-14 21:15:50 -0500

    bullet rate limiting

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.limit = 0
     
     # in the player class
     def triangle(self):
@@ -34,13 +35,16 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.limit < 0:
+                self.shoot()
+        self.limit -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        self.limit = PLAYER_SHOOT_COOLDOWN
         bullet = Shot(self.position.x, self.position.y)
         bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
